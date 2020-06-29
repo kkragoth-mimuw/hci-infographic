e204dc43b85818d109b0d13db1a2368dc8381dd5
kkragoth <kkragoth@gmail.com>
2020-06-29 14:40:34 +0200

    Updates

---
+++ precache-manifest.002aa4a43e4f1e248739c3f1de4d5732.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fb8899af6c5c989f968374b3e2e317",
+    "url": "/hci-infographic/index.html"
+  },
+  {
+    "revision": "b62339ed25d8057e3bfa",
+    "url": "/hci-infographic/static/css/main.db1fa0f3.chunk.css"
+  },
+  {
+    "revision": "4f0e44c79d2a2bd76873",
+    "url": "/hci-infographic/static/js/2.1888c133.chunk.js"
+  },
+  {
+    "revision": "504d6a9b0cf48fda63047afe3d9afd45",
+    "url": "/hci-infographic/static/js/2.1888c133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62339ed25d8057e3bfa",
+    "url": "/hci-infographic/static/js/main.239ec4fb.chunk.js"
+  },
+  {
+    "revision": "e2f7a4988496e9e22769",
+    "url": "/hci-infographic/static/js/runtime-main.c77b1dda.js"
+  },
+  {
+    "revision": "06816e1c2541d8a263c01f6176e60a2b",
+    "url": "/hci-infographic/static/media/1.iPhone_1.06816e1c.png"
+  },
+  {
+    "revision": "6ede642e04b102635e502c0b891da116",
+    "url": "/hci-infographic/static/media/2.IPhone_3G.6ede642e.png"
+  },
+  {
+    "revision": "6ede642e04b102635e502c0b891da116",
+    "url": "/hci-infographic/static/media/3.IPhone_3Gs.6ede642e.png"
+  },
+  {
+    "revision": "418ba628dcad532be35cd55b56078ea2",
+    "url": "/hci-infographic/static/media/4.IPhone_4.418ba628.png"
+  },
+  {
+    "revision": "20a1e4d6fab386ae811982f234d1b222",
+    "url": "/hci-infographic/static/media/5.IPhone4S.20a1e4d6.png"
+  },
+  {
+    "revision": "e83ec53c92b810b0ec403e19f148773d",
+    "url": "/hci-infographic/static/media/6.IPhone_5.e83ec53c.png"
+  },
+  {
+    "revision": "56786aa1952d68dada47626302897eae",
+    "url": "/hci-infographic/static/media/inter-regular.56786aa1.ttf"
+  }
+]);
